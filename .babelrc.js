b0bce21b3884c0b359805f8368f3148ea71d7c12
mderrier <maximilien.derrier@gmail.com>
2020-09-10 17:23:20 +0200

    fix es6

--- .babelrc.js
+++ .babelrc.js
@@ -4,7 +4,7 @@ const env = process.env.NODE_ENV || 'development'
 
 module.exports = factoryBabel({
   env,
-  target: 'web',
+  target: 'library',
   paths: { src: path.resolve('./src'), nodeModules: path.resolve('./node_modules') },
   opts: {
     useUniversal: false,
